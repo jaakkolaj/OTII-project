17f24f98c4352f7c5e9f9568398edb5a502ef8aa
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2026-03-26 16:09:38 +0200

    authentication middleware lisätty supabase routtiin

--- server/src/routes/supabase.route.ts
+++ server/src/routes/supabase.route.ts
@@ -1,10 +1,11 @@
 import { getDocument } from "../controllers/supabase.controller";
 import { Router } from "express";
+import { authentication } from "../middleware/authentication";
 
 // Muodostetaan Router
 const supaBaseRouter = Router();
 
 // Liitetään function supabase routeriin
-supaBaseRouter.get('/:candidate', getDocument);
+supaBaseRouter.get('/:candidate', authentication, getDocument);
 
 export default supaBaseRouter;
