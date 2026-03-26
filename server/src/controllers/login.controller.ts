92fb52655c8bdf99e713b149e17db9d67941384c
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2026-03-26 15:25:46 +0200

    errorhandlerit ai ja candidate routeille

--- server/src/controllers/login.controller.ts
+++ server/src/controllers/login.controller.ts
@@ -6,33 +6,34 @@ import jwt from 'jsonwebtoken';
 import { AuthenticationError } from "../utils/errors";
 
 export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+   try {
     const { email, password } = req.body;
 
-        const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: { email: email }
+    });
 
-        const user = await prisma.user.findUnique({
-            where: { email: email }
-        });
+    const passwordCorrect = user ? await bcrypt.compare(password, user.password) : false;
 
     if(!(user && passwordCorrect)) {
         return next(new AuthenticationError("Invalid email or password"));
     }
 
-        const userForToken = {
-            email: user.email,
-            id: user.id
-        }
+    const userForToken = {
+        email: user.email,
+        id: user.id
+    }
 
-        const token = jwt.sign(userForToken, "kosodpskop");
+    const token = jwt.sign(userForToken, "kosodpskop");
 
-        res.cookie("access_token", token, {
-            httpOnly: true,
-            secure: false,        // vain HTTPS
-            sameSite: "lax",     // suojaa CSRF:ltä
-            maxAge: 60 * 60 * 1000
-        });
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        secure: false,        // vain HTTPS
+        sameSite: "lax",     // suojaa CSRF:ltä
+        maxAge: 60 * 60 * 1000
+    });
 
-        res.status(200).json({ email: user.email, id: user.id, token: token })
+    res.status(200).json({ email: user.email, id: user.id, token: token })
 
     } catch(error) {
         next(error)
